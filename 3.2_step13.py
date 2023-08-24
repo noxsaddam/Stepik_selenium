20e4538463e3341abbe856999a0eccfa335ef2e9
Konstantin <noxsaddam@gmail.com>
2023-08-24 17:55:15 +0300

    изминил степ12 Выполнил тесты 3.2_step13

--- 3.2_step13.py
+++ 3.2_step13.py
@@ -1,41 +1,41 @@
-import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import unittest
 
 
+def get_text(url):
+    with webdriver.Chrome() as browser:
+        browser.get(url)
+        # Ваш код, который заполняет обязательные поля
+        browser.find_element(By.CLASS_NAME, "first_block > .form-group.first_class > .form-control.first").send_keys(
+            "K")
+        browser.find_element(By.CLASS_NAME, "first_block > .form-group.second_class > .form-control.second").send_keys(
+            "M")
+        browser.find_element(By.CLASS_NAME, "first_block > .form-group.third_class > .form-control.third").send_keys(
+            "mail")
 
-class Test(unittest.TestCase): ...
+        # Отправляем заполненную форму
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+        return welcome_text
 
 
-try:
-    link = "http://suninjuly.github.io/registration2.html"
-    browser = webdriver.Chrome()
-    browser.get(link)
+class Test(unittest.TestCase):
+    def test_welcome_text1(self):
+        welcome_text = get_text("http://suninjuly.github.io/registration1.html")
+        self.assertEqual(welcome_text, "Congratulations! You have successfully registered!")
 
-    # Ваш код, который заполняет обязательные поля
-    browser.find_element(By.CLASS_NAME, "first_block > .form-group.first_class > .form-control.first").send_keys("K")
-    browser.find_element(By.CLASS_NAME, "first_block > .form-group.second_class > .form-control.second").send_keys("M")
-    browser.find_element(By.CLASS_NAME, "first_block > .form-group.third_class > .form-control.third").send_keys("mail")
+    def test_welcome_text2(self):
+        welcome_text = get_text("http://suninjuly.github.io/registration2.html")
+        self.assertEqual(welcome_text, "Congratulations! You have successfully registered!")
 
-    # Отправляем заполненную форму
-    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
-    button.click()
 
-    # Проверяем, что смогли зарегистрироваться
-    # ждем загрузки страницы
-    time.sleep(1)
-
-    # находим элемент, содержащий текст
-    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
-    # записываем в переменную welcome_text текст из элемента welcome_text_elt
-    welcome_text = welcome_text_elt.text
-
-    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
-    assert "Congratulations! You have successfully registered!" == welcome_text
-
-finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
-    time.sleep(10)
-    # закрываем браузер после всех манипуляций
-    browser.quit()
+if __name__ == "__main__":
+    unittest.main()
