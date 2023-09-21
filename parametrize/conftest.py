0b275f66d2ceedb71252bc1983232ca3ea6e04c1
Konstantin <noxsaddam@gmail.com>
2023-09-21 16:35:52 +0300

    Перераспределил файлы по папкам

--- parametrize/conftest.py
+++ parametrize/conftest.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
-    browser.implicitly_wait(10)
+    browser.implicitly_wait(5)
     yield browser
     print("\nquit browser..")
     browser.quit()
